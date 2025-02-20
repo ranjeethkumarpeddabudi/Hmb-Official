3e4cd30fd853e3616034cf4584ba96fb51e2299b
ranjeethkumarpeddabudi <110933756+ranjeethkumarpeddabudi@users.noreply.github.com>
2025-02-20 18:23:11 +0530

    Update SignUp.jsx

--- Frontend/src/Components/SignUp.jsx
+++ Frontend/src/Components/SignUp.jsx
@@ -91,7 +91,7 @@ const SignUp = () => {
   return (
     <div className="flex h-full items-center justify-center py-2 m-auto ">
       <form
-        className="flex flex-col px-10 md:px-36 py-20 shadow-2xl bg-[url('bg.jpg')]"
+        className="flex flex-col px-10 md:px-36 py-20 shadow-2xl bg-[url('/bg.jpg')]"
         onSubmit={handleSubmit}
       >
         <img src="hmb-logo.png" alt="" className="h-14 w-14 mx-auto" />
